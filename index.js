fffa7a63e3e99cd8a7080a9203ace74f2b02d6dd
Joanna Jacob <joannajacob910@gmail.com>
2026-02-28 00:31:50 +0530

    fixed the bug in encode fn

--- index.js
+++ index.js
@@ -1,27 +1,38 @@
 const BASE62 = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
 
 const shortToLong = new Map();
-const longToShort = new Map();
 
 let counter = 1;
 
 const BASE_URL = 'http://tinyurl/';
 
-function encode(longUrl){
+const longUrl = document.getElementById("input_link");
+
+const result = document.getElementById("shortened_link");
+
+
+function encode(){
+    const url = longUrl.value;
+
+    if(!url){
+        result.textContent = "please enter a URL :P";
+        return
+    }
+
     const shortKey = convertToBase62(counter);
-    counter ++;
+    counter++;
 
-    shortToLong.set(shortKey, longUrl);
+    shortToLong.set(shortKey, url);
 
-    return BASE_URL + shortKey;
+    result.textContent = BASE_URL + shortKey;
 }
 
 function convertToBase62(num){
-    let result = '';
+    let result_ = '';
     while(num > 0){
         const remainder = num% 62;
-        result = BASE62[remainder] + result;
+        result_ = BASE62[remainder] + result_;
         num = Math.floor(num/62);
     }
-    return result;
+    return result_;
 }
